9797ca3424d06bf639c8cd1a2300cf6ac8cd4433
Corinne Burks <106447520+coburks@users.noreply.github.com>
2023-09-07 18:16:19 -0500

    Create 1_to_n_add_sums_loop.cpp

---
+++ 1_to_n_add_sums_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for (int x = 1; x <= n; x++) {
+        sum += x;
+    }
+    cout << sum << endl;
+
+      
+
+
+
+}
